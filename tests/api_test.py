c8e55e7cde3ee78e21c1f49af707da54420b87bf
Savanni D'Gerinel <savanni@savannidgerinel.com>
2015-09-12 15:20:46 -0500

    Add a test to check that posting a new plan experience works

--- tests/api_test.py
+++ tests/api_test.py
@@ -6,7 +6,7 @@
 
 import spec
 import unittest
-
+import json
 import requests
 
 server_uri = 'http://127.0.0.1:5000'
@@ -33,3 +33,20 @@ class BasicCheck (spec.Spec, unittest.TestCase):
         self.assertEqual(len(r.json()['plans']), 3)
         self.assertIn({'company': 'Humana', 'plan-name': 'blue Advantage Bronze HMO', 'state': 'TX', 'type': 'bronze'}, )
 
+    def test_add_experience (self):
+        payload = {'date': '2015-08-22',
+                   'gender': 'M',
+                   'age': 36,
+                   'plan': 'blue Advantage Bronze HMO',
+                   'company': 'Humana',
+                   'procedure': [
+                        {'name': 'estradiol',
+                         'success': True
+                        },
+                        {'name': 'spironolactone',
+                         'success': True
+                        }]
+                  }
+        r = requests.post('%s/api/v1/experience' % (server_uri,),
+                          data=payload)
+        self.assertEqual(r.status_code, requests.codes.ok)
