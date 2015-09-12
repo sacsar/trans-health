fc4fc38d38259e020e275318262b6b7a86406218
Savanni D'Gerinel <savanni@savannidgerinel.com>
2015-09-12 13:00:33 -0500

    Add relationships and backreferences between database tables

--- src/database.py
+++ src/database.py
@@ -4,7 +4,7 @@
 
 from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, Date, Enum
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy.orm import relationship, sessionmaker, backref
 from sqlalchemy import create_engine
 
 Base = declarative_base()
@@ -27,7 +27,10 @@ class Company (Base):
 class Plan (Base):
     __tablename__ = 'plan'
     id = Column(Integer, primary_key=True)
+
     company_id = Column(Integer, ForeignKey('company.id'), nullable=False)
+    company = relationship(Company, backref=backref('plans', uselist=True))
+
     name = Column(String(250), nullable=False)
     state = Column(String(20), nullable=False)
     color_code = Column(Enum('bronze', 'silver', 'gold', 'platinum', 'catastrophic', 'not-present'),
@@ -39,7 +42,10 @@ class Incident (Base):
     __tablename__ = 'incident'
     id = Column(Integer, primary_key=True)
     date = Column(Date, nullable=False)
+
     plan_id = Column(Integer, ForeignKey('plan.id'), nullable=False)
+    plan = relationship(Plan, backref=backref('incidents', uselist=True))
+
     procedure = Column(String(250), nullable=False)
     stated_gender = Column(Enum('M', 'F', 'U'), nullable=False)
     success = Column(Boolean, nullable=False)
@@ -49,7 +55,10 @@ class CoverageStatement (Base):
     __tablename__ = 'coverage_statement'
     id = Column(Integer, primary_key=True)
     date = Column(Date, nullable=False)
+
     plan_id = Column(Integer, ForeignKey('plan.id'), nullable=False)
+    plan = relationship(Plan, backref=backref('coverage_statements', uselist=True))
+
     procedure = Column(String(250), nullable=False)
     covered = Column(Enum('true', 'false', 'unknown'), nullable=False)
 
@@ -57,6 +66,11 @@ class CoverageStatement (Base):
 class Documents (Base):
     __tablename__ = 'documents'
     id = Column(Integer, primary_key=True)
+
     plan_id = Column(Integer, ForeignKey('plan.id'), nullable=False)
+    plan = relationship(Plan, backref=backref('documents', uselist=True))
+
     coverage_statement_id = Column(Integer, ForeignKey('coverage_statement.id'), nullable=False)
+    coverage_statement = relationship(CoverageStatement, backref=backref('documents', uselist=True))
+
     date = Column(Date, nullable=False)
