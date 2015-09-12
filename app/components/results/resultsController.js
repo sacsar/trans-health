5562c079e5da8d0c5db7fd2b575926fe9c769627
cuyaproductions <diego@cuyaproductions.com>
2015-09-12 18:06:59 -0500

    Throw "error" when no state

--- app/components/results/resultsController.js
+++ app/components/results/resultsController.js
@@ -1,4 +1,4 @@
-transHealthApp.controller('searchController', function ($scope, $location, resultsService) {
+transHealthApp.controller('resultsController', function ($scope, $location, resultsService) {
     
     resultsService.getPlans().then(function (resp){
         $scope.results = resp.data;
