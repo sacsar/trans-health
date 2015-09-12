f9401d802cbe514800f0d38cfb4dfd274a0cd8ff
Sebastian Csar <sacsar@gmail.com>
2015-09-12 16:43:03 -0500

    Fill in searchService to use API

--- app/components/search/searchService.js
+++ app/components/search/searchService.js
@@ -1,8 +1,30 @@
 transHealthApp.service('searchService', function ($http) {
     var service = {};
     
-    service.getPlans = function () {
-        return $http.get('data/search.json');
+    service.getPlan = function (company, name, state) {
+    	var params = {}
+    	params.state = state
+    	params.dimension = 'plan'
+    	params.values = [company, name]
+        return this.searchPlan(params)
+    }
+
+    service.getPlansByCompany = function(company, state) {
+    	var params = {}
+    	params.dimenions = 'company'
+    	params.values = [company]
+    	params.state = state
+    	return this.searchPlan(params)
+    }
+
+    service.getPlans = function(state){
+    	var params = {}
+    	params.state = state
+    	return this.searchPlan(params)
+    }
+
+    service.searchPlan = function(params){
+    	return $http.get('/api/v1/search/' + angular.element.param(params))
     }
     
     return service;
