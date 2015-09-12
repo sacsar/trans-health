f83910a9e0ebd70c82ee264dc311009049f80ada
Savanni D'Gerinel <savanni@savannidgerinel.com>
2015-09-12 08:51:39 -0500

    Placeholder setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "trans-health",
+    version = "0.1.0",
+    packages = find_packages("src/", "tests/"),
+    package_dir = {"": "src"},
+    setup_requires=["nose>=1.0"],
+    install_requires=[
+        "coverage",
+        "spec"
+    ]
+)
