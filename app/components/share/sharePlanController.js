b6ee4dcc100b331bb1d901b72d349747ef150b71
Sebastian Csar <sacsar@gmail.com>
2015-09-13 11:08:31 -0500

    Add share plan page

---
+++ app/components/share/sharePlanController.js
@@ -0,0 +1,62 @@
+transHealthApp.controller('sharePlanController', function($scope, $location, $http){
+	$scope.err = false;
+
+	$scope.state = '';
+	$scope.company = '';
+	$scope.plan = '';
+
+	$scope.allExcluded = '';
+	$scope.hormonesExcluded = '';
+	$scope.surgeryExcluded = '';
+
+	$scope.submitplan = function(){
+		var hormones = '';
+		var medication = '';
+		if($scope.allExcluded == 'yes'){
+			hormones = 'no';
+			surgery = 'no';
+		} else {
+			switch($scope.hormonesExcluded){
+				case 'yes':
+					hormones = 'no';
+					break;
+				case 'no':
+					hormones = 'yes';
+					break;
+				default:
+					hormones = 'unknown';
+					break;
+			}
+
+			switch($scope.surgeryExcluded){
+				case 'yes':
+					surgery = 'no';
+					break;
+				case 'no':
+					surgery = 'yes';
+					break;
+				default:
+					surgery = 'unknown';
+			}
+		}
+
+		coverage_report = {
+			'date': moment().format('YYYY-MM-DD'),
+			'plan': $scope.plan,
+			'company': $scope.company,
+			'state': $scope.state,
+			'service_types': [
+				{'name': 'medication', 'covered': hormones},
+				{'name': 'surgery', 'covered': surgery}
+				]
+		}
+
+		postCoverage(coverage_report)
+	}
+
+	// this should be a service
+	var postCoverage = function(payload){
+		console.log(payload)
+		return $http.post('/api/v1/coverage', payload)
+	}
+});
