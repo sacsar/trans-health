ab21954452971befb37d78d1ddadf6d4dbf4c9b1
Sebastian Csar <sacsar@gmail.com>
2015-09-13 10:09:22 -0500

    Add shareExperience component

---
+++ app/components/share-experience/shareExperienceService.js
@@ -0,0 +1,10 @@
+/*transHealthApp.service('shareExperienceService', function($http){
+	var service = {};
+
+	service.postExperience = function(params){
+
+		$http.post('/api/v1/experience', params)
+	}
+
+	return service
+});*/
